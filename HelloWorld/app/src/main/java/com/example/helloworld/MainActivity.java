ba26fd533f68be73fb8a2c403c7c74fdb414c04c
lintingqian <271657370@qq.com>
2023-09-07 10:27:23 +0800

    helloworld

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("MainActivity", "Verbose");
+        Log.d("MainActivity","Debug");
+        Log.i("MainActivity","Info");
+        Log.w("MainActivity", "Warning");
+        Log.e("MainActivity", "Error");
+        Log.wtf("MainActivity","Assert");
+    }
+}
